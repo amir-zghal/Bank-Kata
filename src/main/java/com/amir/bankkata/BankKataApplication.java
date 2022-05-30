6cc37af77a9e13d2f0f7d0d38f13461dca87b30d
Amir Zghal <amir.zghal@gmail.com>
2022-05-30 10:15:27 +0200

    Project initialization

---
+++ src/main/java/com/amir/bankkata/BankKataApplication.java
@@ -0,0 +1,13 @@
+package com.amir.bankkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankKataApplication.class, args);
+    }
+
+}
